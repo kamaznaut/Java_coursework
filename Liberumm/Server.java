48d3c6d25274ce35e8f39d8969af2834876b46ac
kamaznaut <kamilparonoa@gmail.com>
2025-12-05 07:06:34 +0700

    односторонняя отправка

--- Liberumm/Server.java
+++ Liberumm/Server.java
@@ -1,17 +1,36 @@
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
 
 public class Server {
     public static void main(String[] args) throws IOException {
         int port = 12345;
+
+
         ServerSocket serverSocket = new ServerSocket(port);
-        System.out.println("Server started on port " + port);
+        System.out.println("Server started");
 
 
         Socket client = serverSocket.accept();
-        System.out.println("Client connected: " + client.getRemoteSocketAddress());
+        System.out.println("Client connected");
+
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+
+
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+
+
+        while (true) {
+            System.out.print("Server > ");
+            String msg = console.readLine();
+            out.write(msg + "\n");
+            out.flush();
+
+
+            if (msg.equals("/quit")) break;
+        }
 
 
         client.close();
