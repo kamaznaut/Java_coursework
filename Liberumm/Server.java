17a647d77daaaaf48e915e46b2bee9631618b8bb
kamaznaut <kamilparonoa@gmail.com>
2025-12-26 17:27:47 +0700

    добавил регестрацию на сервер через map а так же команду /list для отображения онлайна

--- Liberumm/Server.java
+++ Liberumm/Server.java
@@ -1,47 +1,22 @@
-import java.io.*;
-import java.net.*;
-
-public class Server {
-    public static void main(String[] args) {
-        // Создаю сокет слушает порт 1234
-        try (ServerSocket serverSocket = new ServerSocket(1234)) {
-            System.out.println("Сервер запущен и ждет подключений...");
-
-            while (true) {
-                // accept() ждет пока не подключится новый чел
-                Socket socket = serverSocket.accept();
-                System.out.println("Новое подключение!");
-
-                // создаюобъект-обработчик и запускаю в новом потоке
-                // Это позволяет серверу сразу вернуться к accept()
-                new Thread(new ClientHandler(socket)).start();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
-
-// Класс, который будет общаться с конкретным клиентом в своем потоке
-class ClientHandler implements Runnable {
-    private Socket socket; // Сокет конкретного клиента
-
-    public ClientHandler(Socket socket) {
-        this.socket = socket;
-    }
-
-    @Override
-    public void run() {
-        try (// Получаем поток ввода, чтобы читать, что пишет клиент
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-
-            String msg;
-            // Читаем сообщения, пока клиент не отключится
+String msg;
             while ((msg = in.readLine()) != null) {
-                System.out.println("Сообщение от клиента: " + msg);
-            }
-        } catch (IOException e) {
-            System.out.println("Клиент отключился.");
+        // Если клиент ввел /list
+        if (msg.equalsIgnoreCase("/list")) {
+        // Превращаем список ключей Map в одну строку и отправляем клиенту
+        out.println("Сейчас в сети: " + String.join(", ", Server.clients.keySet()));
         }
-    }
-}
+        // Если сообщение начинается с @ (например @Ivan привет)
+        else if (msg.startsWith("@")) {
+String[] parts = msg.split(" ", 2); // Делим строку на 2 части: [@Ivan] и [привет]
+String targetName = parts[0].substring(1); // Убираем символ @, получаем "Ivan"
+
+// Ищем обработчик Ивана в нашей Map
+ClientHandler target = Server.clients.get(targetName);
+                    if (target != null && parts.length > 1) {
+        // Отправляем сообщение напрямую в поток вывода Ивана
+        target.out.println("[Личное от " + clientName + "]: " + parts[1]);
+                    } else {
+                            out.println("Пользователь " + targetName + " не найден.");
+                    }
+                            }
+                            }
